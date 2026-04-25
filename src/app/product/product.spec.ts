b9034cdb8eb77c3f2f2cc20c420ec096e9321633
Patil, Yashwanth <patil_yashwanth@epam.com>
2026-04-25 19:34:07 +0530

    yaml added

--- src/app/product/product.spec.ts
+++ src/app/product/product.spec.ts
@@ -1,6 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Product } from './product';
+import { User } from '../../services/user';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+class MockUserService {
+  getUsers() {
+    return of([]);
+  }
+}
+
+const storeMock = {
+  pipe: () => of([]),
+  dispatch: () => {}
+};
+
+const activatedRouteMock = {
+  // Add properties/methods if your component uses them, e.g.:
+  snapshot: {},
+  params: of({}),
+  queryParams: of({})
+};
 
 describe('Product', () => {
   let component: Product;
@@ -9,6 +30,11 @@ describe('Product', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Product],
+      providers: [
+        { provide: User, useClass: MockUserService },
+        { provide: Store, useValue: storeMock },
+        { provide: ActivatedRoute, useValue: activatedRouteMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Product);
--- src/directives/background-color.spec.ts
+++ src/directives/background-color.spec.ts
@@ -1,8 +1,10 @@
+import { ElementRef } from '@angular/core';
 import { BackgroundColor } from './background-color';
 
 describe('BackgroundColor', () => {
   it('should create an instance', () => {
-    const directive = new BackgroundColor();
+   const el = { nativeElement: document.createElement('div') } as ElementRef;
+    const directive = new BackgroundColor(el);
     expect(directive).toBeTruthy();
   });
 });
